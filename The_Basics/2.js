865c8b02eeac55355575a82a894f2fab92f33bfe
Erik Lind <erik@Administrators-Mac-mini.local>
2020-05-24 23:53:12 -0500

    small change

--- The_Basics/2.js
+++ The_Basics/2.js
@@ -1,16 +1,16 @@
 let num = 4936;
 
-let ones = num % 10
-console.log(`ones place is ${ones}`)
+let ones = num % 10;
+console.log(`ones place is ${ones}`);
 
-num = (num - ones) / 10
-let tens = num % 10
-console.log(`tens place is ${tens}`)
+num = (num - ones) / 10;
+let tens = num % 10;
+console.log(`tens place is ${tens}`);
 
-num = (num - tens) / 10
-let hundreds = num % 10
-console.log(`hundreds is ${hundreds}`)
+num = (num - tens) / 10;
+let hundreds = num % 10;
+console.log(`hundreds is ${hundreds}`);
 
-num = (num - hundreds) / 10
-let thousands = num % 10
-console.log(`thousands is ${thousands}`)
+num = (num - hundreds) / 10;
+let thousands = num % 10;
+console.log(`thousands is ${thousands}`);
